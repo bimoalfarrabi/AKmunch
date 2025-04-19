1e99bbae04c1d3c63018ebf07bce6c0a8c0018ba
Viktor Fedorov <impqxr@gmail.com>
2025-04-19 19:43:16 +0200

    variants

--- anykernel.sh
+++ anykernel.sh
@@ -32,7 +32,50 @@ ramdisk_compression=auto;
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 750 750 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+# Auto‑detect variant from zip name
+case "$ZIPFILE" in
+  *-miui*)    v=miui;;
+  *-ir*)      v=ir;;
+  *N0Kernel*) v=default;;
+esac
+
+# If none are detected (adb sideload), let the user pick
+if [ -z "$v" ]; then
+  set -- miui ir default
+  i=1; n=$#
+  prev_option=""
+  ui_print "Select DTBO variant:"
+  while :; do
+    eval "current_option=\${$i}"
+    # Only print when the option changes
+    if [ "$current_option" != "$prev_option" ]; then
+      ui_print "> Option selected: $current_option  (Vol–=Next  Vol+=Select)"
+      prev_option="$current_option"
+    fi
+    ev=$(getevent -lc1 2>/dev/null | tr -d '\r')
+    case $ev in
+      *KEY_VOLUMEDOWN*DOWN*)
+        i=$(( i % n + 1 ))
+        ;;
+      *KEY_VOLUMEUP*DOWN*)
+        v="$current_option"
+        break
+        ;;
+    esac
+    sleep 0.1
+  done
+fi
+
+# Select default if still unset
+[ -z "$v" ] && v=default
+
+# Apply the right dtbo
+ui_print " • Using $v DTBO"
+if [ "$v" != default ]; then
+  rm -f dtbo.img && mv "$v/dtbo.img" "dtbo.img"
+fi
 
 ## AnyKernel install
 dump_boot;
